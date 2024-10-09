d52fab6bca3c9c1928b5ca18ea2f5228bc6b38ac
azizktata <65952489+azizktata@users.noreply.github.com>
2024-10-09 20:53:51 +0100

    updates

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,10 +1,10 @@
 import { createClient } from 'next-sanity'
 
 export const client = createClient({
-  projectId: "l7whyd9g",
-  dataset: "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2022-03-07",
   useCdn: true,
-  token: "skiv2BTjJJBYd4JNz74gy5t7NBGNLnLL37nWzukDBn7i3aGwhjhjcYv9DTb0BxQGHFsodTmwhJWGrLN8yVrPuY7YlAWmVeQje8M89fnaX0xDsCEc4QxxRYRXv5X8qxP4aw4lS2idZYbA1MzC50zwPAG2YBZQT6eZx8jgSBWOJOxIOAGcbhs6"
+  token: process.env.TOKEN
   // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
