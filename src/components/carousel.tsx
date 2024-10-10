986ab992ce48b0ece0bc4dc48329c364dc03c2d3
azizktata <65952489+azizktata@users.noreply.github.com>
2024-10-10 21:29:52 +0100

    updates

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -15,7 +15,7 @@ const urlFor = (source: SanityImageSource, width = 1920, height = 1080) =>
         .image(source)
         .width(width)
         .height(height)
-        .quality(70)
+        .quality(50)
         .format("webp")
         .auto("format")
     : null;
